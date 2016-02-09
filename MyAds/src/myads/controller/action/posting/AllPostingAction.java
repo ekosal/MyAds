f35d6fe87782b34b10e4eb10dee9339f882874d1
ekosal2014 <ekosal2014@gmail.com>
2016-02-09 23:28:07 +0700

    data

--- MyAds/src/myads/controller/action/posting/AllPostingAction.java
+++ MyAds/src/myads/controller/action/posting/AllPostingAction.java
@@ -18,7 +18,7 @@ public class AllPostingAction implements Action {
 		try{
 			
 			request.getSession().setAttribute("listAllPosting", dao.listAllPosting());			
-			
+			System.out.println("Hello");
 			forward.setRedirect(false);
 			forward.setPath("mynewposts.jsp");
 			return forward;
