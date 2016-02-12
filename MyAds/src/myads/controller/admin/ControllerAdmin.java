b396931c352ffa0306f9781803542090b3361a8a
ekosal2014 <ekosal2014@gmail.com>
2016-02-12 16:16:20 +0700

    asdfghjkl; dfghjkl;

--- MyAds/src/myads/controller/admin/ControllerAdmin.java
+++ MyAds/src/myads/controller/admin/ControllerAdmin.java
@@ -458,6 +458,7 @@ public class ControllerAdmin extends HttpServlet {
 	/**.
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doProccess(request, response);
@@ -466,6 +467,7 @@ public class ControllerAdmin extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doProccess(request, response);
